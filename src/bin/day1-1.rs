8a9d85199b2a43431efd13826b2ae879c552e6bc
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-12-15 17:27:37 +0900

    day1-1 solved

--- src/bin/day1-1.rs
+++ src/bin/day1-1.rs
@@ -1,4 +1,18 @@
 fn main() {
-    let my_str = include_str!("../../inputs/day1-1");
-    print!("{my_str}");
+    let inputs = include_str!("../../inputs/day1-1");
+    let lines = inputs.lines();
+    let sum: u32 = lines.map(num_by_line).sum();
+    println!("{:?}", sum)
+}
+
+fn num_by_line(line: &str) -> u32 {
+    let mut numbers = line.chars().filter(|c| c.is_ascii_digit());
+    let first_num = numbers.next().unwrap();
+    if let Some(number) = numbers.last() {
+        let combined = first_num.to_string() + &number.to_string();
+        combined.parse().unwrap()
+    } else {
+        let combined = first_num.to_string() + &first_num.to_string();
+        combined.parse().unwrap()
+    }
 }
